fad5293f865d0ba511ff02c937ec55e488bd81d4
wajid ansari <112863100+ansariwajid9999@users.noreply.github.com>
2024-10-14 17:37:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const change_button = document.getElementById("change_button")
+const Reset = document.getElementById("Reset")
+
+change_button.onclick = () => {
+    const block_id = document.getElementById("block_id").value
+    const color_id = document.getElementById("color_id").value
+
+    reset()
+
+    if(block_id >= "1" && block_id <= "9" && color_id){
+        const selectedBox = document.getElementById(block_id)
+        selectedBox.style.backgroundColor = color_id
+    }
+}
+
+Reset.onclick = () => {
+    reset()
+}
+
+function reset(){
+    const gridItems = document.querySelectorAll(".grid-item")
+
+    for(let item of gridItems){
+        item.style.backgroundColor = 'transparent'
+    }
+}
